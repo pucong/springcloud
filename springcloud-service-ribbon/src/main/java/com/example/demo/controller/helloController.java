222c29592238a5c1fa8c7660f8ad61d15f2b258b
pucong <906886111@qq.com>
2017-08-01 23:56:34 +0800

    初始化
    123

---
+++ springcloud-service-ribbon/src/main/java/com/example/demo/controller/helloController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by PC on 2017/8/1.
+ */
+@RestController
+public class helloController {
+    @Autowired
+    HelloService helloService;
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam String name){
+        return helloService.hiService(name);
+    }
+}
---
+++ springcloudClientOne/src/main/java/com/pc/controller/systemController.java
@@ -0,0 +1,22 @@
+package com.pc.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by PC on 2017/8/1.
+ */
+@RestController
+public class systemController {
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi "+name+",i am from port:" +port;
+    }
+
+}
